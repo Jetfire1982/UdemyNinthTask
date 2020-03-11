4c8b4d00c3b696200674093a4816eb7c81a62cb1
Petr <Mclaren-f1@rambler.ru>
2020-03-11 10:50:04 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', function () {
     "use strict";
+    //Tabs
+
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -13,18 +15,18 @@ window.addEventListener('DOMContentLoaded', function () {
 
     hideTabContent(1);
 
-    function showTabContent(b){
-        if (tabContent[b].classList.contains('hide')){
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('show');
         }
     }
 
-    info.addEventListener('click', function(event){
-        let target=event.target;
-        if(target&&target.classList.contains('info-header-tab')){
-            for(let i=0; i<tab.length; i++){
-                if(target==tab[i]){
+    info.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
                     hideTabContent(0);
                     showTabContent(i);
                     break;
@@ -34,4 +36,56 @@ window.addEventListener('DOMContentLoaded', function () {
 
     });
 
+    //Timer
+    let deadline = '2020-03-21';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            total: t,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            if (t.seconds.toString().length==1)
+            seconds.textContent = '0'+t.seconds;
+            else
+            seconds.textContent = t.seconds;
+
+            if (t.minutes.toString().length==1)
+            minutes.textContent = '0'+t.minutes;
+            else
+            minutes.textContent = t.minutes;
+
+            if (t.hours.toString().length==1)
+            hours.textContent = '0'+t.hours;
+            else
+            hours.textContent = t.hours;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+
+            }
+        }
+    }
+
+    setClock('timer', deadline);
 });
